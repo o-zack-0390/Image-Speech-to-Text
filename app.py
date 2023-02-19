3e2311c6c9273de9b5ac15d3b15a384329c28fdd
o-zack-0390 <116938721+o-zack-0390@users.noreply.github.com>
2023-02-19 22:30:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,6 @@ st.write("exists(): " + str(os.path.exists(cur_dir)))
 st.write("isdir(): " + str(os.path.isdir(cur_dir)))
  
 st.write("isfile(): " + str(os.path.isfile(cur_dir)))
+
+tools = pyocr.get_available_tools()
+tool = tools[0]
