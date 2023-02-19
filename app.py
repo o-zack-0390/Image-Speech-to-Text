ea0e02cc65bd155dee77b826c87b7b7211fa5c23
o-zack-0390 <116938721+o-zack-0390@users.noreply.github.com>
2023-02-19 22:28:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
-from PIL import Image
+import streamlit as st
 import pyocr
+from PIL import Image
+
 
 cur_dir = "tesseract.exe"
 
-print("exists(): " + str(os.path.exists(cur_dir)))
+st.write("exists(): " + str(os.path.exists(cur_dir)))
  
-print("isdir(): " + str(os.path.isdir(cur_dir)))
+st.write("isdir(): " + str(os.path.isdir(cur_dir)))
  
-print("isfile(): " + str(os.path.isfile(cur_dir)))
+st.write("isfile(): " + str(os.path.isfile(cur_dir)))
