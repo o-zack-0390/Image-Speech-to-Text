826da92f86d802c7ccb1ea086d8c0390de7466b0
o-zack-0390 <116938721+o-zack-0390@users.noreply.github.com>
2023-02-20 11:31:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,35 @@ import streamlit as st
 import pyocr
 from PIL import Image
 
-path               = "/usr/share/tesseract-ocr"
-os.environ['PATH'] = os.environ['PATH'] + path
-tools              = pyocr.get_available_tools()
-tool               = tools[0]
-uploaded_file      = st.file_uploader("ファイルアップロード", type='jpg')
 
-if uploaded_file:
+def Image_to_Text():
+   path               = "/usr/share/tesseract-ocr"
+   os.environ['PATH'] = os.environ['PATH'] + path
+   tools              = pyocr.get_available_tools()
+   tool               = tools[0]
+   uploaded_file      = st.file_uploader("ファイルアップロード", type='jpg')
+   
+   if uploaded_file:
     
-    if st.button("テキストに変換"):
+      if st.button("テキストに変換"):
 
-      image   = Image.open(uploaded_file)
-      builder = pyocr.builders.TextBuilder(tesseract_layout = 6)
-      text    = tool.image_to_string(image, lang = "jpn", builder = builder)
+        image   = Image.open(uploaded_file)
+        builder = pyocr.builders.TextBuilder(tesseract_layout = 6)
+        text    = tool.image_to_string(image, lang = "jpn", builder = builder)
 
-      with st.expander("Image to Text"):
-        st.write(text)
+        with st.expander("Image to Text"):
+          st.write(text)
+
+
+def Speech_to_Text():
+  pass
+
+
+st.markdown("## Image&Speech to Text")
+
+if st.selectbox("変換方法を選択",[ "Image to Text", "Speech to Text"]) == "Image to Text":
+  Image_to_Text()
+
+else:
+  Speech_to_Text()
+
