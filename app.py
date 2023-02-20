5fba9cd10a7f996cdc62b2a1486a7e2c00135a2c
o-zack-0390 <116938721+o-zack-0390@users.noreply.github.com>
2023-02-20 11:20:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,44 +3,19 @@ import streamlit as st
 import pyocr
 from PIL import Image
 
-
-cur_dir = "/usr/share/tesseract-ocr/4.00/tessdata"
-
-#"/usr/share/tesseract-ocr"
-
-st.write("exists(): " + str(os.path.exists(cur_dir)))
-st.write("isdir(): " + str(os.path.isdir(cur_dir)))
-st.write("isfile(): " + str(os.path.isfile(cur_dir)))
-st.write(os.listdir(cur_dir))
-
-path = "/usr/share/tesseract-ocr"
+path               = "/usr/share/tesseract-ocr"
 os.environ['PATH'] = os.environ['PATH'] + path
-tools = pyocr.get_available_tools()
-tool = tools[0]
-
-st.write(tool)
-
-uploaded_file=st.file_uploader("ファイルアップロード", type='jpg')
+tools              = pyocr.get_available_tools()
+tool               = tools[0]
+uploaded_file      = st.file_uploader("ファイルアップロード", type='jpg')
 
 if uploaded_file:
     
-    if st.button("test start"):
-      image=Image.open(uploaded_file)
+    if st.button("テキストに変換"):
 
-      '''
-      TESSERACT_PATH = "usr/share/tesseract-ocr"
-      TESSDATA_PATH  = "usr/share/tesseract-ocr/4.00/tessdata/jpn.traineddata"
+      image   = Image.open(uploaded_file)
+      builder = pyocr.builders.TextBuilder(tesseract_layout = 6)
+      text    = tool.image_to_string(image, lang = "jpn", builder = builder)
 
-      st.write(os.environ["PATH"])
-
-      os.environ['PATH'] += os.pathsep + TESSERACT_PATH
-      os.environ["TESSDATA_PREFIX"] = TESSDATA_PATH
-
-      st.write(os.environ['PATH'])
-      st.write(os.environ["TESSDATA_PREFIX"])
-      '''
-
-      builder = pyocr.builders.TextBuilder(tesseract_layout=6)
-      text = tool.image_to_string(image, lang="jpn", builder=builder)
-
-      st.write(text)
+      with st.expander("Image to Text"):
+        st.write(text)
